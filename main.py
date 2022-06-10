2d375431e27031c823fcd8102be12c40aa67a745
Astra Berry <astraberry82@gmail.com>
2022-06-10 23:17:04 +0000

    Added comments

--- main.py
+++ main.py
@@ -27,9 +27,8 @@ scissors = '''
 
 #Write your code below this line 👇
 import random
-
 game = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. \n"))
-
+#user input for game
 if game == 0:
   print(rock)
 elif game == 1:
@@ -37,8 +36,10 @@ elif game == 1:
 else:
   print(scissors)
 
+#calling for the computer
 print("Computer")
 
+#making computer choice random
 computer_choice = [0, 1, 2]
 random_choice = random.choice(computer_choice)
 
@@ -48,14 +49,14 @@ elif random_choice == 1:
   print(paper)
 else:
   print(scissors)
-
+#calling all of the ways for a draw
 if game == 0 and random_choice == 0:
   print("Draw!")
 elif game == 1 and random_choice == 1:
   print("Draw")
 elif game == 2 and random_choice == 2:
   print("Draw!")
-
+#telling the user if they won or lost against computer in 3 different if, elif statements
 if game == 0 and random_choice == 1:
   print("You Lose!")
 elif game == 1 and random_choice == 0:
@@ -71,5 +72,6 @@ if game == 2 and random_choice == 0:
 elif game == 0 and random_choice == 2:
   print("You Win!")
 
+#making sure no numbers other than the valid inputs will work
 if game >= 3 or game < 0:
   print("You typed an invalid number. You Lose!")
